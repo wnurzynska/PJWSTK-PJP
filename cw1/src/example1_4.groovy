2e05efc2c757a5816deec5cebdb60b302836486c
Michał Cywiński <mcywinski@hotmail.com>
2018-01-01 19:07:29 +0100

    Przerobienie zadań z ćwiczenia 1 do stylu groovy/skryptowego.

--- cw1/src/example1_4.groovy
+++ cw1/src/example1_4.groovy
@@ -1,70 +1,66 @@
 // Napisać pogram przekształcający liczby na ich słowne odpowiedniki.
 // Np. po wprowadzeniu liczby 147 winniśmy uzyskać jeden - cztery - siedem. (5)
 
-class example1_4 {
-    static void main(String[] args) {
-        def scanner = new Scanner(System.in)
+def scanner = new Scanner(System.in)
 
-        print "Podaj liczby:"
-        def nums = scanner.nextLine()
+print "Podaj liczby:"
+def nums = scanner.nextLine()
 
-        if (!isNumeric(nums)) {
-            println("Wartość nienumeryczna!")
-            return
-        }
+if (!isNumeric(nums)) {
+    println("Wartość nienumeryczna!")
+    return
+}
 
-        displayAsWords(nums)
+displayAsWords(nums)
+
+def isNumeric(String text) {
+    return text.toCharArray().every { it.isDigit() }
+}
+
+def displayAsWords(String rawNumbers) {
+    def numbers = rawNumbers.collect { x -> Integer.parseInt(x) }
+    numbers.eachWithIndex{ number, index ->
+        displayAsWord(number, index == 0)
+    }
+}
+
+def displayAsWord(int number, boolean isFirst) {
+    def word = ""
+
+    switch (number) {
+        case 0:
+            word = "zero"
+            break
+        case 1:
+            word = "jeden"
+            break
+        case 2:
+            word = "dwa"
+            break
+        case 3:
+            word = "trzy"
+            break
+        case 4:
+            word = "cztery"
+            break
+        case 5:
+            word = "pięć"
+            break
+        case 6:
+            word = "sześć"
+            break
+        case 7:
+            word = "siedem"
+            break
+        case 8:
+            word = "osiem"
+            break
+        case 9:
+            word = "dziewięć"
+            break
     }
 
-    static boolean isNumeric(String text) {
-        return text.toCharArray().every { it.isDigit() }
-    }
-
-    static void displayAsWords(String rawNumbers) {
-        def numbers = rawNumbers.collect { x -> Integer.parseInt(x) }
-        numbers.eachWithIndex{ number, index ->
-            displayAsWord(number, index == 0)
-        }
-    }
-
-    static void displayAsWord(int number, boolean isFirst) {
-        def word = ""
-
-        switch (number) {
-            case 0:
-                word = "zero"
-                break
-            case 1:
-                word = "jeden"
-                break
-            case 2:
-                word = "dwa"
-                break
-            case 3:
-                word = "trzy"
-                break
-            case 4:
-                word = "cztery"
-                break
-            case 5:
-                word = "pięć"
-                break
-            case 6:
-                word = "sześć"
-                break
-            case 7:
-                word = "siedem"
-                break
-            case 8:
-                word = "osiem"
-                break
-            case 9:
-                word = "dziewięć"
-                break
-        }
-
-        if (!isFirst)
-            print(" - ")
-        print(word)
-    }
+    if (!isFirst)
+        print(" - ")
+    print(word)
 }
--- cw1/src/example1_6.groovy
+++ cw1/src/example1_6.groovy
@@ -1,56 +1,52 @@
 // Napisy wprowadzane w kolejnych dialogach mają formę:  pozycja = koszt.
 // Zsumuj wszystkie koszty dla tych samych pozycji (np. mleko, chleb). (5)
 
-class example1_6 {
-    static void main(String[] args) {
-        def items = collectItems()
-        items.each { key, value ->
-            println "$key: $value"
+def items = collectItems()
+items.each { key, value ->
+    println "$key: $value"
+}
+
+def collectItems() {
+    def map = [:]
+
+    while (true) {
+        def rawProduct = getRawProduct()
+        if (rawProduct == "")
+            break
+
+        def parts = rawProduct.split(" = ")
+        def product = parts[0]
+        def price = new BigDecimal(parts[1])
+
+        if (map.containsKey(product)) {
+            def sum = map.get(product)
+            sum += price
+            map.replace(product, sum)
+        }
+        else
+            map.put(product, price)
+    }
+
+    return map
+}
+
+def getRawProduct() {
+    def prompt = true
+    def scanner = new Scanner(System.in)
+    def input = ""
+
+    while (prompt) {
+        prompt = false
+        print "Podaj produkt: "
+        input = scanner.nextLine()
+        if (input == "")
+            break
+
+        if (!input.matches("[a-zA-Z]* = [0-9]*.[0-9]{2}")) {
+            prompt = true
+            println "Błędny zapis produktu. Spróbuj ponownie..."
         }
     }
 
-    static LinkedHashMap collectItems() {
-        def map = [:]
-
-        while (true) {
-            def rawProduct = getRawProduct()
-            if (rawProduct == "")
-                break
-
-            def parts = rawProduct.split(" = ")
-            def product = parts[0]
-            def price = new BigDecimal(parts[1])
-
-            if (map.containsKey(product)) {
-                def sum = map.get(product)
-                sum += price
-                map.replace(product, sum)
-            }
-            else
-                map.put(product, price)
-        }
-
-        return map
-    }
-
-    static String getRawProduct() {
-        def prompt = true
-        def scanner = new Scanner(System.in)
-        def input = ""
-
-        while (prompt) {
-            prompt = false
-            print "Podaj produkt: "
-            input = scanner.nextLine()
-            if (input == "")
-                break
-
-            if (!input.matches("[a-zA-Z]* = [0-9]*.[0-9]{2}")) {
-                prompt = true
-                println "Błędny zapis produktu. Spróbuj ponownie..."
-            }
-        }
-
-        return input
-    }
+    return input
 }

3114aa6a87da2184e1f181d6368372ee6bae26b5
Michał Cywiński <mcywinski@hotmail.com>
2018-01-01 17:26:43 +0100

    Rozwiązania ćw.1.

---
+++ cw1/src/example1_2.groovy
@@ -0,0 +1,7 @@
+// Napisać w wybranym edytorze tekstowym program typu "hello world" w Groovy. Uruchomić go z konsoli. (2)
+
+class example1_2 {
+    static void main(String[] args) {
+        println("Hello world!")
+    }
+}
